930d8e209a99505e071ea3cbdad1a0f12c4dd007
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-17 09:42:50 -0400

    taskes language C,variable

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
 int main(void)
-
+/**
+  *main - Entry point
+  *Return: Always 0 (succsess)
+  */
 {
 int i, j, k;
 
 for (i = 0; i <= 8; i++)
- {
+{
 for (j = i + 1; j <= 9; j++)
-  {
+{
 k = i * 10 + j;
 putchar(k / 10 + '0');
 putchar(k % 10 + '0');
 if (i < 8)
-   {
+{
 putchar(',');
 putchar(' ');
-   }
-  }
- }
+}
+}
+}
 putchar('\n');
 return (0);
 }

1015f4eb1819d341cc15117ca0dff445388fa9eb
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-17 10:09:37 -0400

    taskes language C,variable

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
-
-    int main(void)
+/**
+ *main - Entry point
+ *Return: Always 0 (succsess)
+ */
+int main(void)
     {
-    int i, j, k;
+int i, j, k;
 
-    for (i = 0; i < 8; i++)
-    {
-    for (j = i + 1; j < 9; j++)
-    {
-    for (k = j + 1; k < 10; k++)
-    {
-    putchar(i + '0');
-    putchar(j + '0');
-    putchar(k + '0');
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
 
-    if (i == 7 && j == 8 && k == 9)
-    break;
+if (i == 7 && j == 8 && k == 9)
+break;
 
-    putchar(',');
-    putchar(' ');
-    }
-    }
-    }
+putchar(',');
+putchar(' ');
+}
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
-    }
+return (0);
+}

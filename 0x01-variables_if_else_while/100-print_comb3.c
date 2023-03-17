ecae103d5bbf868cccc0dac7fb368d8099a87d4a
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-17 08:09:52 -0400

    taskes language C,variable

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,33 +2,33 @@
 
 int main(void)
 {
-int i, j;
+    int i, j;
 
-for (i = 0; i < 100; i++)
-{
-for (j = i + 1; j < 100; j++)
-{
-if ((i / 10 == j / 10) || (i % 10 == j % 10) || (i == j))
-{
-continue;
-}
+    for (i = 0; i < 100; i++)
+    {
+        for (j = i + 1; j < 100; j++)
+        {
+            if ((i / 10 == j / 10) || (i % 10 == j % 10) || (i == j))
+            {
+                continue;
+            }
 
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(',');
-putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(',');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
 
-if (i != 98 || j != 99)
-{
- <putchar(',');
-putchar(' ');
-}
-}
-}
+            if (i != 98 || j != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-putchar('\n');
-return 0;
+    putchar('\n');
+    return 0;
 }
 

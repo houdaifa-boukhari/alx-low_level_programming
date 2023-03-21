5e330e0bf37559dad26a9762c17f50e12b067b26
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-21 12:14:43 -0400

    taskes language C,debugging

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: the number of times to print the table (0 <= n <= 15)
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: the number to print the times table for
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
-	int i, j;
+    if (n < 0 || n > 15)
+        return;
 
-	if (n < 0 || n > 15)
-		return;
+    int i, j;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++) 
-		{
-			int res = i * j;
-
-			if (j == 0)
-				_putchar('0');
-			else
-				printf("%3d", res);
-			if (j != n)
-				_putchar(',');
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            int result = i * j;
+            if (j == 0)
+                printf("%d", result);
+            else
+                printf(", %d", result < 100 ? result < 10 ? 3 : 2 : 1);
+        }
+        printf("\n");
+    }
 }

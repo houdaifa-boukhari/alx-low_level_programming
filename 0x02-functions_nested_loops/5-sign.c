134c8e87007050a7ea158ebc7bd3b880e32b26f6
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-21 10:20:06 -0400

    taskes language C,Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
  * print_sign - prints the sign of a number
  * @n: number to check
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * Return: 1 if n is greater than zero
+ , 0 if n is zero, -1 if n is less than zero
  */
 int print_sign(int n)
 {
@@ -15,7 +16,7 @@ return (1);
 else if (n == 0)
 {
 _putchar('0');
-return 0;
+return (0);
 }
 else
 {

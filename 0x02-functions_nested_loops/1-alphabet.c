ef6699f7d0da14c2872c747913b9ff7d6a04a1f2
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-21 07:40:07 -0400

    taskes language C,Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+  /**
+ * main - Write a function that prints the alphabet
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+    
+    _putchar('\n');
+}

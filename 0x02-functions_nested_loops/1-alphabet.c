76a312bf6f18abbbc5cdb45c15109c6c57561ef1
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-21 08:02:58 -0400

    taskes language C,Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
-  /**
- * print_alphabet - prints the lowercase alphabet
- *
- * Return: Always 0 (success) 
- */
+/**
+* print_alphabet - prints the alphabet in lowercase.
+*
+* Return: Always 0 (success) 
+*/
 void print_alphabet(void)
 {
 char letter = 'a';

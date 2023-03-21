7296898bd30fe822f4019227958d97f5749b847e
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-21 10:02:59 -0400

    taskes language C,Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,10 +5,10 @@
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
- int _islower(int c)
+int _islower(int c)
 {
- if (c >= 'a' && c <= 'z')
- return 1;
- else
- return 0;
+if (c >= 'a' && c <= 'z')
+return 1;
+else
+return 0;
 }

758a378c128a1f3e2b7b3fe6ccf5c784b814dfaf
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-23 14:58:22 -0400

    taskes language C,function more

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * mul - multiply 2 int
- * @a: first int
- * @b: second int
- * Return: addition of the multiplicattion
+ * @a: first integer
+ * @b: second integer
+ * Return: always 0
  *      
  */
 
 int mul(int a, int b)
 {
 	return (a * b);
+	_putchar('\n');
 }

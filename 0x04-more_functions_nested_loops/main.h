0cb8052079303b4b66b6991e564b933d694c7cf8
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-23 11:41:40 -0400

    taskes language C,function more

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 int _putchar(char c);
 int _isupper(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isdigit(int c);
+#endif /* MAIN_H */
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);

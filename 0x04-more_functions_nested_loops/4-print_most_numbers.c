ee54013cf0cac4417c3692be052c027cb5f9fd5d
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-23 15:18:51 -0400

    taskes language C,function more

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - print num except 2 and 4
  * Return: void
- *   
+ *
  **/
 
 void print_most_numbers(void)
@@ -13,10 +13,11 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 
 	{
-								
+
 		if (!(c == '2' || c == '4'))
-									_putchar(c);
-							
+		_putchar(c);
+					
 	}
-		_putchar('\n');
+
+	_putchar('\n');
 }

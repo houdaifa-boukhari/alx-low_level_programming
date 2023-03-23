c5000bda6b173df08daca0f577f6faecbf4eca68
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-03-23 11:24:37 -0400

    taskes language C,function more

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,13 +7,14 @@
  * Return: 1 if c is a digit, 0 otherwise
  */
 
-int _isdigit(int c)
+int _isdigit(int x)
 {
-	if (c >= '0' && c <= '9')
+	if (x >= 48 && x <= 57)
+	{
 
 		return (1);
 
-	else
-					
+	}					
 		return (0);
+	
 }

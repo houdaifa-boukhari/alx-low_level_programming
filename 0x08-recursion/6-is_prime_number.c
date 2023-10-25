ae6239927f5d2ce0601b12c6010853a95abfcce5
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-10-25 22:58:39 +0100

    my_project

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,8 +15,8 @@ int	check_prime_number(int n, int i)
 	if (i > n / 2)
 		return (1);
 	if (n % i == 0)
-		return (0)
-	return (is_prime_number(n, i + 1));
+		return (0);
+	return (check_prime_number(n, i + 1));
 }
 
 /**

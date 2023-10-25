196ff374234d6c0bfcd006c820e3f90c6965923d
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-10-25 22:55:28 +0100

    my_project

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,20 +3,31 @@
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
+ *@i: the test
  *
  * Return: 1 if n is a prime number, 0 if not
  */
 
-int	actual_prime(int n, int i)
+int	check_prime_number(int n, int i)
 {
-	i = 2;
-	if (n < 2)
+	if (i < 2)
 		return (0);
-	while (i <= n / 2)
-	{
-		if (n % i == 0)
-			return (0);
-		i++;
-	}
-	return (1);
+	if (i > n / 2)
+		return (1);
+	if (n % i == 0)
+		return (0)
+	return (is_prime_number(n, i + 1));
+}
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+
+int	is_prime_number(int n)
+{
+	return check_prime_number(n, n / 2);
+	
 }

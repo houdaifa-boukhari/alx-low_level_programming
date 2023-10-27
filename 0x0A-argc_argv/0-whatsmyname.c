30bcd4959013c20610b68bca269fafcfb49df86b
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-10-27 11:20:09 +0100

    my_project

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-/*
+/**
 * main - prints the first argumment
-*@argc: number of argumment
-*@argv: array of argumment
-*Return: always 0
+* @argc: number of argumment
+* @argv: array of argumment
+*
+* Return: always 0
 */
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }

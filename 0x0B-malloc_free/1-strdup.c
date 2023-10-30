b5b0740e3a4cea33c5dc4933c3eec3f97a87b700
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-10-30 15:04:57 +0100

    my_projects

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strdup - copy string in new string allocated
+* @str: string to copy in new string allocated
+*
+* Return: NEw str.
+*/
+
+char *_strdup(char *str)
+{
+	int i;
+	char *new_str;
+
+	i = strlen(str) + 1;
+	new_str = (char *)malloc(sizeof(char) * i);
+	if (new_str == NULL)
+		return (NULL);
+	new_str = strcpy(new_str, str);
+	return (new_str);
+}

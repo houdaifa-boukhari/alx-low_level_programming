00256b004bb7f554895cc1b230ab0f93d13179c4
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-10-30 16:56:45 +0100

    my_projects

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* free_grid - free allocation array 2 dimensional
+* @height: height of the grid
+* @grid: array 2 dimenstion
+*
+* Return: void.
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	if (!grid)
+		return;
+	while (i < height)
+	{
+		if (grid[i])
+			free(grid[i]);
+	}
+	free(grid);
+}

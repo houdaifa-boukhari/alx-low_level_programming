3548605200d06d0f42a8bc90291ec30656b535d8
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-10-30 17:42:19 +0100

    my_projects

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* argstostr - concatenates all the arguments of your program
+* @av: strings
+* @ac: number of strings
+*
+* Return: new string.
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int len_str = 0;
+	int total_len = 0;
+	char *str;
+
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	i = 1;
+	while (i < ac)
+	{
+		total_len += strlen(av[i]);
+		i++;
+	}
+	str = (char *)malloc(sizeof(char) * (total_len + ac));
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			str[len_str] = av[i][j];
+			len_str++;
+			j++;
+		}
+		if (i < ac - 1)
+			str[len_str++] = '\n';
+		i++;
+	}
+	str[len_str] = '\0';
+	return (str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,5 +10,6 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
 #endif

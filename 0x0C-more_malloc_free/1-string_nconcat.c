73060e7034d5e0e184a755287506df7bb67c742c
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-02 21:07:02 +0100

    my_projects

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,16 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
+	int i;
 	int j;
-	int lenght_ncontact = 0;
 	char *str;
 
 	s1 == NULL ? s1 = "" : s1;
 	s2 == NULL ? s2 = "" : s2;
-	while (s2[i] != '\0' && n--)
-		i++;
-	lenght_ncontact = strlen(s1) + i + 1;
-	str = (char *)malloc(sizeof(char) * lenght_ncontact);
+	if (n >= strlen(s2))
+		n = strlen(s2);
+	i = strlen(s1) + n + 1;
+	str = (char *)malloc(sizeof(char) * i);
 	if (!str)
 		return (NULL);
 	i = 0;

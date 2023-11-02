c17c592a85a22ecfc6a25cfe78d439abc0bfac03
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-02 21:18:32 +0100

    my_projects

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _calloc - allocate memory in new string
+ * @nmemb: number the element
+ * @size: the size of the memory to allocate
+ *
+ * Return: pointto by string.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void	*memory;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	memory = (void)malloc(size * nmemb);
+	if (memory == NULL)
+		return (NULL);
+	ft_memset(memory, 0, size * nmemb);
+	return (memory);
+}

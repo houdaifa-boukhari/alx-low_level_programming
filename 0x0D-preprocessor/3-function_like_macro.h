b2c0f7f7f146cc14188f22c9806b463da84098e1
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-06 19:36:59 +0100

    my_projects

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-# define ABS(x) \
-    ({ \
-        int abs_value = (x); \
-        if (abs_value < 0) { \
-            abs_value = -abs_value; \
-        } \
-        abs_value; \
-    })
+#define ABS(x) ((x) > 0 ? (x) : (-(x)))
 
 #endif

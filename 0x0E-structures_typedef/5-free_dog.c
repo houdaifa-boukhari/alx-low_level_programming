488ad41dd39ce47bb489fbec7829353cfbdd38b4
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-06 18:49:45 +0100

    my_projects

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - free element dog
+ * @d: pownter point by element the structer
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+}

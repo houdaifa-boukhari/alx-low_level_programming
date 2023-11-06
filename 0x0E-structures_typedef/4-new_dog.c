7d047bc83d40fe2324a933605556cf2d0e559521
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-06 18:36:33 +0100

    my_projects

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,10 +17,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	d->name = (char *)malloc(sizeof(char) * sizeof(name));
 	if (!(d->name))
+	{
+		free(d);
 		return (NULL);
+	}
 	d->owner = (char *)malloc(sizeof(char) * sizeof(owner));
 	if (!(d->owner))
+	{
+		free(d->name);
+		free(d);
 		return (NULL);
+	}
 	d->name = strcpy(d->name, name);
 	d->age = age;
 	d->owner = strcpy(d->owner, owner);

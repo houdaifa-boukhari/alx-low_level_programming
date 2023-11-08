b817f5be19349c2224c7f432d74dda7a6a80abfb
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-08 12:57:53 +0100

    my_projects

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}

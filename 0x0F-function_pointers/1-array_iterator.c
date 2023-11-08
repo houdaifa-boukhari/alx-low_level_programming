540ed7c290fbeb2641cca40fb2435e68e1971645
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-08 13:11:11 +0100

    my_projects

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints a array_iterator
+ * @array: les element to edit
+ * @size: size to array
+ * @action: pointer to element to edit
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+		action(array[i++]);
+}

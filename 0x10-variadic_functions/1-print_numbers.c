c8a93fb59507ac2e8134695dde52bbe195c8c0cc
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-09 16:59:34 +0100

    my_projects

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print_numbers passed a parametr
+ * @separator: sep
+ * @n:  is the number of integers passed to the function
+ *
+ * Return: NOTHINGe
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i;
+
+	if (!separator)
+		return;
+	va_start(args, n);
+	i = 0;
+	while (i < (int)n - 1)
+	{
+		printf("%d%s", va_arg(args, int), separator);
+		i++;
+	}
+	printf("%d\n", va_arg(args, int));
+	va_end(args);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+#endif

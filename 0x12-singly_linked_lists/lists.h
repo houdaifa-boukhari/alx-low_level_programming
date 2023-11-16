c2ffd4218a308ce218789af0c4e61658dd6e799c
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-16 16:09:50 +0100

    My project

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef LISTS_h
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdio.h>
@@ -11,13 +11,14 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 
-#endif 
+#endif

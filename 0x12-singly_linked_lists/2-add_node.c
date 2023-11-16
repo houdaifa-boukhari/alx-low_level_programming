899017620cca5fbeba49fca1cd308321a0961522
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-16 16:50:59 +0100

    My project

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,9 +16,14 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = (list_t *)malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	while (str[i])
 		i++;
-	new_node->str = strdup(str);
 	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;

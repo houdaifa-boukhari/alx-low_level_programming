925122f034f5ce87efda8efc1e3cc465d299c2b7
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-20 20:41:10 +0100

    My project

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,34 @@
 #include "lists.h"
 
+/**
+ * add_node_begenner - adds a new node at the beginning of a list
+ * @head: pointer of the header linked list
+ * @n: integer to store in new node
+ * Return: the new node
+ */
+
+listint_t *add_node_begenner(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
+/**
+ * insert_nodeint_at_index - inserting a new node at agiven postion
+ * @head: double pointer
+ * @idx: index of the node
+ * @n: data to store in new node
+ *
+ * Return: return head
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr1;
@@ -13,7 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 	{
-		add_nodeint(&(*head), n);
+		add_node_begenner(&(*head), n);
 		return (*head);
 	}
 	new_node->n = n;

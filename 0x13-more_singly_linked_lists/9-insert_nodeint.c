3c4ca2c78ec2a5e6875efa44b51a1edf775cee87
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-20 20:58:18 +0100

    My project

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *ptr2;
 
-	if (!head)
+	if (!head || !(*head))
 		return (NULL);
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (!new_node)

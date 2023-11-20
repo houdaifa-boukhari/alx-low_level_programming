22501d8e5eb1a63bc269064a34d4158b9779887a
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-20 20:53:51 +0100

    My project

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,12 +13,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *ptr1;
 	listint_t *ptr2;
 
-	if (!head)
+	if (!head || !(*head))
 		return (-1);
 	ptr1 = *head;
 	if (index == 0)
 	{
-		*head = (*head) ->next;
+		*head = (*head)->next;
 		free(ptr1);
 		return (1);
 	}
@@ -29,6 +29,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	ptr2 = ptr1->next->next;
 	free(ptr1->next);
-	ptr1->next= ptr2;
+	ptr1->next = ptr2;
 	return (1);
 }

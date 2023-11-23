437103b166e9ac33fdf707db5fef2bc2188a7698
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-23 18:18:23 +0100

    My project

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flib_bits - returns the number of bits you would need to flip
+ * @n: number1
+ * @m: number2
+ * Return: returns the number of bits you would need to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int nb;
+	int i;
+
+	nb = n ^ m;
+	i = 0;
+	while (nb)
+	{
+		i += nb & 1;
+		nb >>= 1;
+	}
+	return (i);
+}

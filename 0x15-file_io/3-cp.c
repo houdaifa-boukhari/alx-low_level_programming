c9c674967ec64fcb08d6c319c7ee4d1d9ec2af7f
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-27 21:39:57 +0100

    My project

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,44 @@
 #include "main.h"
 
 /**
+* creat_buffer - creat buffer to 1024 byte
+ * @filename: The name file
+ *
+ * Return: void
+ */
+
+void	creat_buffer(char *filename)
+{
+	char *buffer;
+
+	buffer = (char *)malloc(sizeof(char) * 1024);
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", filename);
+		exit(99);
+	}
+}
+
+/**
+* close_file - close file.
+ * @filename: name the file
+ *
+ * Return: void
+ */
+
+void close_file(char *filename)
+{
+	int nb;
+
+	nb = close(filename);
+	if (nb == -1)
+	{
+		dprintf(2, "Error: Can't close fd FD_VALUE");
+		exit(100);
+	}
+}
+
+/**
 * main - copies the content of a file to another file.
  * @argc: The number of argument.
  * @argv: argument.
@@ -21,31 +59,28 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
-		exit(99);
-	}
-	buffer = (char *)malloc(1024 * sizeof(char));
+	buffer = creat_buffer(argv[2]);
 	bytes_read = read(fd1, buffer, 1024);
-	bytes_write = write(fd2, buffer, 1024);
-	if (bytes_write == -1 || bytes_read == -1 ||
-			bytes_read != bytes_write)
-	{
-		free(buffer);
-		return (-1);
-	}
-	if (close(fd2) == -1 || close(fd1) == -1)
-	{
-		dprintf(2, "Error: Can't close fd FD_VALUE");
-		exit(100);
-	}
-	free (buffer);
+	do {
+		if (fd1 == -1 || bytes_read  == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+		bytes_write = write(fd2, buffer, 1024);
+		if (bytes_write == -1 || fd2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		bytes_read = read(fd1, buffer, 1024);
+		fd2 = open(argv[2], O_WRONLY | O_APPEND);
+	} while (bytes_read > 0);
+	free(buffer);
+	close_file(fd1);
+	close_file(fd2);
 	return (0);
 }

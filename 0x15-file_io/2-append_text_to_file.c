e4ce2243ade62ba0a75473a2595d0c5d7eac6a46
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-27 17:45:50 +0100

    My project

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* append_text_to_file - that appends text at the end of a file.
+ * @filename: The name of the file to create.
+ * @text_content: A NULL-terminated string to write to the file.
+ * Return: 1 on success, -1.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int bytes_write;
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (-1);
+	}
+	bytes_write = write(fd, text_content, strlen(text_content));
+	if (bytes_write == -1 || bytes_write != (int)strlen(text_content))
+	close(fd);
+	return (1);
+}

ed26ae2f42404775b5a3014f8ef97bef070fa8fc
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-03-24 18:10:01 +0000

    push

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -57,11 +57,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = NULL;
 	dlistint_t *position = NULL;
-    unsigned int size = lst_size(*h);
+	unsigned int size = lst_size(*h);
 
-    if (!h || idx > size)
+	if (!h || idx > size)
 		return (NULL);
-    else if (idx == 0)
+	else if (idx == 0)
 		return (add_dnodeint(h, n));
 	else if (idx == size)
 		return (add_dnodeint_end(h, n));

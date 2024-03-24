c48f37a909e1a83dc075203158da7e2932fe9359
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-03-24 23:34:33 +0000

    push

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -95,4 +95,52 @@ int	delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	position = NULL;
 	return (1);
 }
+int main(void)
+{
+    dlistint_t *head = NULL;
 
+    // Test deleting from an empty list
+    printf("Deleting from an empty list:\n");
+    int res = delete_dnodeint_at_index(&head, 0);
+    printf("return %d\n", res);
+
+    // Add a single node to the list
+    add_dnodeint_end(&head, 0);
+
+    // Test deleting the only node in the list
+    printf("Deleting the only node in the list:\n");
+    res = delete_dnodeint_at_index(&head, 0);
+    printf("return %d\n", res);
+
+    // Add some nodes to the list
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+
+    // Test deleting the first node
+    printf("Deleting the first node:\n");
+    res = delete_dnodeint_at_index(&head, 0);
+    printf("return %d\n", res);
+    print_dlistint(head);
+
+    // Test deleting a middle node
+    printf("Deleting a middle node:\n");
+    res = delete_dnodeint_at_index(&head, 1);
+    printf("return %d\n", res);
+    print_dlistint(head);
+
+    // Test deleting the last node
+    printf("Deleting the last node:\n");
+    res = delete_dnodeint_at_index(&head, lst_size(head) - 1);
+    printf("return %d\n", res);
+    print_dlistint(head);
+
+    // Test deleting with an out-of-bounds index
+    printf("Deleting with an out-of-bounds index:\n");
+    res = delete_dnodeint_at_index(&head, lst_size(head));
+    printf("return %d\n", res);
+	system("leaks a.out");
+
+    return (0);
+}

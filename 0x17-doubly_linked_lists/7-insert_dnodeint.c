46a7cda33426366f77754ee2b810c870fef04082
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-03-24 18:13:53 +0000

    push

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -70,6 +70,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = position->next;
 	position->next = new_node;
 	new_node->prev = position;
-	new_node->next->prev = new_node;
+	if (new_node->next)
+		new_node->next->prev = new_node;
 	return (new_node);
 }

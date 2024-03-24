196cb240ea38f50598022f7eeab1607155544ff1
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-03-24 16:08:33 +0000

    push

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,7 +28,7 @@ dlistint_t	*add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	while (ptr->next)
 		ptr = ptr->next;
-	new_node->prev = ptr->next;
+	new_node->prev = ptr;
 	ptr->next = new_node;
 	return (*head);
 }

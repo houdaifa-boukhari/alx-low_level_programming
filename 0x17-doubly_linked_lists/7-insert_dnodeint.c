8ab401044c67a5a6ba174e35aea9ec1e75443c9e
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-03-24 18:05:39 +0000

    push

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
+
 /**
- * get_index - returns the nth node of a dlistint_t linked list
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
  * @head: pointer to the head of the list
  * @index: index of the node to return
  *
@@ -22,28 +23,6 @@ dlistint_t *get_index(dlistint_t *head, unsigned int index)
 }
 
 /**
- * dlistint_size - returns the number of elements in a doubly linked list
- * @h: pointer to the head of the list
- *
- * Return: number of elements in the list
- */
-
-unsigned int dlistint_size(dlistint_t *h)
-{
-	unsigned int count;
-
-	count = 0;
-	if (!h)
-		return (0);
-	while (h)
-	{
-		h = h->next;
-		count++;
-	}
-	return (count);
-}
-
-/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: pointer to the head of the list
  * @idx: index of the list where the new node should be added
@@ -56,19 +35,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = NULL;
 	dlistint_t *position = NULL;
-	// unsigned int size = dlistint_size(*h);
 
-	// if (!h || idx > size || idx < 0)
-	// 	return (NULL);
-	// if (idx == 0)
-	// 	return (add_dnodeint(h, n));
-	// else if (idx == size)
-	// 	return (add_dnodeint_end(h, n));
 	new_node = add_dnodeint_end(&new_node, n);
-	position = get_index(*h, idx - 1);
+	position = get_index(*h, idx -1);
 	new_node->next = position->next;
 	position->next = new_node;
 	new_node->prev = position;
 	new_node->next->prev = new_node;
-	return (NULL);
+	return (*h);
 }

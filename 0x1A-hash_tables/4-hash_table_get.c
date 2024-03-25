39fccaa0b228c7e4e5088aa880c1c4de9d21719e
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-03-25 16:19:48 +0000

    push

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to look for
+ * Return: value associated with the element, or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t		*ptr;
+
+	if (!ht || !ht->array|| ht->size == 0 || !key)
+		return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[idx];
+	while (ptr)
+	{
+		if (strcmp(ptr->key, key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (NULL);
+}

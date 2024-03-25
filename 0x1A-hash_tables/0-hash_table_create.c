96788b534065707f3121cb15d76f6fd91775e506
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-03-25 01:19:20 +0000

    push

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = NULL;
-    unsigned long int i = 0;
+	hash_table_t *hash_table = NULL;
+	unsigned long int i = 0;
 
-    hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
-    if (!hash_table)
-        return (NULL);
-    hash_table->size = size;
-    hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * (size));
-    if (!hash_table->array)
-    {
-        free(hash_table);
-        return (NULL);
-    }
-    while (i < size)
-        hash_table->array[i++] = NULL;
-    return (hash_table);
+	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!hash_table)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * (size));
+	if (!hash_table->array)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	while (i < size)
+		hash_table->array[i++] = NULL;
+	return (hash_table);
 }
